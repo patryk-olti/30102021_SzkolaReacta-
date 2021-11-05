8bd9cba78ded3af81a4dcbba480ea38c41bb71f4
Patryk Oltuch <95oltuchpatryk@gmail.com>
2021-11-05 18:17:02 +0100

    installed gsap and created hello animation for home page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,80 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+
+import 'typeface-roboto';
+
+import { gsap } from 'gsap';
 
 const Home = () => {
 
+    const dotRef = useRef();
+    const tl = gsap.timeline();
+
+    const styles = {
+        container: {
+            position: 'relative',
+            width: '100%',
+            height: '100vh',
+            overflow: 'hidden'
+        },
+        dot: {
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            height: '50px',
+            width: '50px',
+            borderRadius: '50%',
+            backgroundColor: 'white',
+            maxHeight: '100vh'
+        },
+        text: {
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            fontFamily: 'roboto',
+            lineHeight: '200%'
+        },
+        background: {
+            position: 'absolute',
+            width: '100%',
+            height: '100vh',
+            backgroundColor: 'black',
+            zIndex: '-1'
+        }
+    }
+
+    useEffect( () => {
+        tl.set(dotRef.current, {
+            scale: 0.1
+        })       
+        .to(dotRef.current, {
+            scale: 3,
+            duration: 1,
+            ease: 'bounce'
+        })
+        .to(dotRef.current, {
+            scale: 40,
+            duration: 2
+        })
+    }, [])
+
 
     return(
-        <div>
-            xyz
+        <div style={styles.container}>
+
+            <div style={styles.background} ></div>
+
+            <div style={styles.dot} ref={dotRef}></div>
+
+            <div style={styles.text}> What is Lorem Ipsum?
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+
+                Why do we use it?
+                It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+            </div>
+
+            
         </div>
     )
 }
